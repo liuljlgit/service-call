bc722d6f0089e34daa6847f042194fc1e5ffd4cc
刘立俊 <645604899@qq.com>
2020-06-20 18:24:59 +0800

    Initial commit

---
+++ provider-server-client/src/main/java/com/cloud/personal/providerserverclient/ProviderServerClientApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.personal.providerserverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderServerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderServerClientApplication.class, args);
+    }
+
+}
---
+++ provider-server-client/src/test/java/com/cloud/personal/providerserverclient/ProviderServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.personal.providerserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderServerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
